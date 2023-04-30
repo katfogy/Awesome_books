c92ec41ad68f4409bfa63f04744e0f8e35c1c7d2
katfogy <katfogy@gmail.com>
2023-04-30 14:53:39 +0100

    awesome book with es6

---
+++ modules/viewAllBooks.js
@@ -0,0 +1,13 @@
+export default function displayBooks() {
+    const contactSession = document.getElementById('contact');
+    const bookSession = document.getElementById('books-all');
+    const addBookSession = document.getElementById('add-books');
+    const listMenu = document.getElementById('list-menu');
+
+    listMenu.addEventListener('click', () => {
+        contactSession.style.display = 'none';
+        bookSession.style.display = 'block';
+        addBookSession.style.display = 'none';
+    });
+
+}
